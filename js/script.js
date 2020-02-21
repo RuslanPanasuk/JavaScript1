b98db9ba0d2feec987220cb554e585cfa58f64a2
RuslanPanasuk <59254482+RuslanPanasuk@users.noreply.github.com>
2020-02-21 23:28:10 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var str = 10;
+var a = 5;
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+
+var number =5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+console.log(4/0);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: true
+}
+
+console.log(persone.name);
+
+let arr = ['Ruslan', 'Slava', 'Sergey'];
+
+console.log(arr[2]);
